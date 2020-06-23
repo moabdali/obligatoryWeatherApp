b60ce40363cec2e03fcd6edbfb597c505a1bf30d
moabdali <64987634+moabdali@users.noreply.github.com>
2020-06-23 05:31:35 -0500

    added more frames

--- ObligatoryWeatherApp.py
+++ ObligatoryWeatherApp.py
@@ -27,7 +27,7 @@ json_object = response.json()
 jsonFormatted = json.dumps(json_object["results"][0]["geometry"]["location"], indent=2)
 
 
-#print(jsonFormatted)
+
 
 a = json_object["results"][0]["geometry"]["location"]["lat"]
 b = json_object["results"][0]["geometry"]["location"]["lng"]
@@ -45,55 +45,55 @@ response = requests.get(url)
 json_object = response.json()
 
 jsonFormatted = json.dumps(json_object, indent=2)
-#print(jsonFormatted)
+
 
 forecastList = []
 num = 0
 for i in json_object["properties"]["periods"]:
-    #print(i['name']+": \n"+i["detailedForecast"]+"\n\n")
     forecastList.append( (i['name'],i["detailedForecast"],i["icon"]))
-    #print(f"forcastList is: {forecastList}")
 
 
 
 
 
-
-
-print(forecastList[0][2])
+print(f"Length: {len(forecastList)}")
+for i in range(0,14):
+    print(f"{i}; {forecastList[i]}")
 
 layout = []
 column = []
-layout.append( [sg.Text("Obligatory Weather API App",key="title", font = "cambria 30", text_color = "Blue")])
+layout+= [[sg.Text("Obligatory Weather API App",key="title", font = "cambria 30", text_color = "Blue")]]
+layout+= [sg.Input("What's your city and state?", visible = True),sg.Button("OK"),sg.Combo(['Normal', 'Detailed','Tiny'])],
 frame_layout =[]
-for i in range(0,14):
+frame_layout2 = []
+frame_layout2+=[[sg.Text("textBox0",size = (25,12),font = "cambria 12",key="textBox0"),sg.Image(data = "", key="image0")]]
+
+for i in range(1,14):
+    print(f"textBox is {[i]}")
     frame_layout += [
         [
-                  [sg.Text("a",size = (12,25),font = "cambria 12",key=f"textBox{i}")],
+                  [sg.Text(f"textBox{i}",size = (25,12),font = "cambria 12",key=f"textBox{i}")],
                   [sg.Image(data = "", key=f"image{i}")]
            ]       
                ]
 
+layout+= [  [sg.Frame(forecastList[0][0], frame_layout2, font = 'Any 12', title_color='green')for i in range(0,1)]]
+layout+= [  [sg.Frame(forecastList[i][0], frame_layout[i-1], font='Any 12', title_color='blue')for i in range(1,14,2)]]
+layout+= [  [sg.Frame(forecastList[i][0], frame_layout[i-1], font='Any 12', title_color='blue')for i in range(2,13,2)]]
 
-layout+= [  [sg.Frame(forecastList[i][0], frame_layout[i], font='Any 12', title_color='blue')for i in range(0,14)]]  
 
-##column2=[    ]
+window = sg.Window("a",layout,grab_anywhere=True)
+window.finalize()
 
-
-#layout+=column1
-#layout+=column2
-
-window = sg.Window("a",layout,finalize = True,grab_anywhere=True)
-
-for i in range (0,14):
-    window[f"textBox{i}"].Update(forecastList[i][1])
-    print(forecastList[i][0])
-    url = forecastList[i][2]
-    #print(forecastList[i][2])
-    response = requests.get(url)
-    img = (response.content)
-    window[f"image{i}"].Update(data=img)
+while True:
+    window['textBox0'].update("hello")
+    for i in range (0,14):
+        window[f"textBox{i}"].Update(forecastList[i][1])
+        #print(forecastList[i][0])
+        url = forecastList[i][2]
+        #print(forecastList[i][2])
+        response = requests.get(url)
+        img = (response.content)
+        window[f"image{i}"].Update(data=img)
                                
-
-
-window.read()
+    window.read()
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,9 @@
 A simple weather app to play around with more APIs and GUI practice
 
-v.0.3 - frames add, resized properly.  Todo: allow user to pick their city (somewhat implemented already, just need to add to GUI and uncomment the code), allow user to show a "mini mode" that only shows the day and picture, consolidate the program so that the day/night stack vertically (this will reduce the horizontal space, but increase the vertical space)
+
+v.0.4 - more frames added (better formatting) including option to type city (doesn't function yet) and to select a mode.  Have not actually implemented the changes after you choose a mode.  Need a logic switch for how to format it depending on whether it's day or night (might be easier to just suppress day 14 if it's already night so you don't only see a loose day for the 14th day.
+
+v.0.3 - frames added, resized properly.  Todo: allow user to pick their city (somewhat implemented already, just need to add to GUI and uncomment the code), allow user to show a "mini mode" that only shows the day and picture, consolidate the program so that the day/night stack vertically (this will reduce the horizontal space, but increase the vertical space)
 
 v.02 - horizontal, but needs frames
 
